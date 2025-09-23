50740a4c0147acb96bf95197d546fbd107e07afa
Veniaminwhs <veniamin208879@student-schoolsofwestfield.org>
2025-09-23 12:12:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-/* Variable and Alert Katas */
-// These katas do not involve functions and are very basic. 
-
-/* COLOR KATAS */
-
-/* Var Alert */
-// make a string variable for a color, set it equal to your favorite
-// alert the message" "My favorite color is " plus the color
-var color = "grean";
 /* Var Alert Prompt */
-alert ( "My favorite color is " + color );
-// make a string variable for color, prompt the user for their favorite
-// alert color " is a nice color!"
-
-/* Var Alert Prompt Conditional */
-// make a variable for color, prompt the user for their favorite
-// if user enters "black" alert "Black is not really a color." 
-// otherwise alert color " is a nice color!"
+// make a variable for a pet type, prompt user for it 
+var petType = prompt("what pet do you have ?");
+// make a variable for a pet name, prompt user for it
+var petName = prompt("what is you call him ?");
+// alert "You have a pet type named pet name 
+alert("You have " + petType + " named " + petName );
+/* Var Alert Prompt Conditionals */
+// make a variable for pet type, prompt user for it 
+// make a variable for pet name, prompt user for it
+// alert "You have a pet type named pet name 
+// if pet is a dog, say "I like dogs, too!"
+// if it is a cat, say "I'm allergic to cats"
+// If it is not a dog or a cat, say "what an interesting pet!"g
